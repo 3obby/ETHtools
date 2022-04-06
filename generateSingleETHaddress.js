804f82db5270af7cdad8fdaf8afb7aed0e9d26ca
3obby <96895030+3obby@users.noreply.github.com>
2022-04-05 19:56:27 -0500

    Add files via upload

---
+++ generateSingleETHaddress.js
@@ -0,0 +1,11 @@
+var keythereum = require("keythereum");
+
+var dk = keythereum.create();
+
+var keyObject = keythereum.dump('password', dk.privateKey, dk.salt, dk.iv);
+
+var computePrivateKey=keythereum.recover('password',keyObject);
+var privateKey = computePrivateKey.toString('hex');
+
+console.log('Address:                    '+ keyObject.address);
+console.log('PK: '+privateKey);
